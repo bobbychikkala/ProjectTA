6c5b8e7a7bec2a7672e5d9d6ec03e86f5595d107
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-07-08 21:43:41 +0530

    intial

---
+++ src/test/java/package1/MakeMyTrip.java
@@ -0,0 +1,86 @@
+package package1;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+
+
+
+public class MakeMyTrip {
+	static WebDriver driver = new ChromeDriver();
+	@BeforeTest
+	public void launchingBrowser(){
+		driver.get("https://www.makemytrip.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.xpath("//*[@data-cy='closeModal']")).click();	
+	}
+
+	@AfterTest
+	public void closingBrowser() { 
+		driver.quit();
+	}
+
+
+	@Test(dataProvider ="cities")
+	public void testLeastPrice(String from,String to) throws IOException {
+		driver.findElement(By.id("fromCity")).sendKeys(from);
+		driver.findElement(By.xpath("//*[@class='makeFlex column flexOne']")).click();
+		driver.findElement(By.id("toCity")).sendKeys(to);
+		driver.findElement(By.xpath("//*[@class='makeFlex column flexOne']")).click();
+
+		int leastPrice = Integer.MAX_VALUE;		//27456566677
+		String leastPriceofDate="";
+		String priceXpath ="//*[@class=' todayPrice']";
+		List<WebElement> prices = driver.findElements(By.xpath(priceXpath));
+		List<WebElement> dates = driver.findElements(By.xpath(priceXpath+"/../.."));
+		
+		HashMap<String,Integer> priceWithDates = new HashMap<String,Integer>();
+		System.out.println();
+		
+		for (int i=1 ; i<prices.size();i++){
+			
+			int price =Integer.parseInt(prices.get(i).getText().replace(",", ""));//3000
+			String eachDate = dates.get(i).getDomAttribute("aria-label");
+			priceWithDates.put(eachDate, price); // this is extra --storing date and price in dictionary
+			if (price<leastPrice) {
+				leastPrice =price ;
+				leastPriceofDate = eachDate;
+				
+				
+			}
+			
+		}
+		
+		System.out.println("From "+from +" to "+to + " Flights");
+		System.out.println("Cheapest flight Price is :"+leastPrice);
+		System.out.println("Dated on " +leastPriceofDate);
+		//dates.get(k).click();
+	}
+
+	
+	@DataProvider(name ="cities")
+	public String[][] getData(){
+		return new String [][] {
+			{"Hyderabad","Bengaluru"}, 
+			{"Bengaluru","Hyderabad"}, 
+			{"Bengaluru","Chennai"},	
+			{"Chennai","Delhi"},
+			{"Mumbai","Delhi"}, 
+			{"Kolkata","Delhi"}
+				 
+		};
+	}
+}
---
+++ src/test/java/package1/WebTable.java
@@ -0,0 +1,76 @@
+package package1;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import java.time.Duration;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+public class WebTable {
+	static WebDriver driver = new ChromeDriver();
+	static String innings;
+	
+	@DataProvider(name="inningNo")
+	public  String[][]dataSet(){
+		String url1="https://www.cricbuzz.com/live-cricket-scorecard/106673/rsa-vs-aus-final-icc-world-test-championship-final-2025";
+		String url2="https://www.cricbuzz.com/live-cricket-scorecard/118586/sl-vs-ban-1st-test-bangladesh-tour-of-sri-lanka-2025";
+		return   new String[][]{
+			{"innings_1",url1},
+			{"innings_2",url1},
+			{"innings_3",url1},
+			{"innings_4",url1},
+			{"innings_1",url2},
+			{"innings_2",url2},
+			{"innings_3",url2},
+			};
+	}
+
+	@Test(dataProvider ="inningNo",priority=1)
+	public  void testCricketScore(String inning,String url) {
+		driver.get(url);
+		innings=inning;
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String commonXpath ="//div[@id='"+innings+"']/div[1]";
+		int batsmen = driver.findElements(By.xpath(commonXpath+"/div/div[1]/a")).size();
+		int column = driver.findElements(By.xpath(commonXpath+"/div[3]/div")).size();
+		
+		
+
+		for (int r=2 ; r<=batsmen+2;r++) {		
+			for(int c=1;c<column;c++) {
+				
+				String data =driver.findElement(By.xpath(commonXpath+"/div["+r+"]/div["+c+"]")).getText();
+				System.out.print(data);
+				
+				if(c<2) for(int e=0;e<30-data.length();e++)System.out.print(" ");
+				
+				else if(c==2) for(int d=0;d<45-data.length();d++)System.out.print(" ");
+		
+				else  for(int e=0;e<6-data.length();e++)System.out.print(" ");
+				
+			}
+			System.out.println();
+		}
+		AssertJUnit.assertTrue(true);
+		//driver.quit();
+		System.out.println("===========================================================================================================");
+	
+	}
+	
+	
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+	}
+	
+	
+}
