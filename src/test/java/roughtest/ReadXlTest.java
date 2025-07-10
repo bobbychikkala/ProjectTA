6fead6d9f2a39708b5ab8442166014d5ae6e847a
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-07-10 14:56:23 +0530

    new

--- src/test/java/roughtest/ReadXlTest.java
+++ src/test/java/roughtest/ReadXlTest.java
@@ -9,10 +9,8 @@ public class ReadXlTest {
 	public static void main(String[]args) {
 		
 		String path = "./src/test/resources/testData.xlsx"  ;
-		System.out.println(ExcelReader.getCellValue(path, "Sheet1"));
+		System.out.println(ExcelReader.dataProvider(path, "Sheet1"));
 	
-		
-
 
 	}
 }
